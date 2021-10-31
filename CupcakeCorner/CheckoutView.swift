6555900886c2c09808c315c6c3020aa201d9ba76
JulyKikuAkita <b92701105@gmail.com>
2021-10-30 18:21:37 -0700

    Sending and receiving orders over the internet

--- CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CheckoutView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct CheckoutView: View {
     @ObservedObject var order: Order
+    @State private var confirmationMessage = ""
+    @State private var showingConfimation = false
 
     var body: some View {
         GeometryReader { geo in
@@ -23,7 +25,7 @@ struct CheckoutView: View {
                         .font(.title)
 
                     Button("Place order") {
-                        // place the order
+                        self.placeOrder()
 
                     }
                     .padding()
@@ -36,6 +38,39 @@ struct CheckoutView: View {
             }
         }
         .navigationBarTitle("Check out", displayMode: .inline)
+        .alert(isPresented: $showingConfimation) {
+            Alert(title: Text("Thank you!"), message: Text(confirmationMessage), dismissButton: .default(Text("OK")))
+        }
+    }
+
+    func placeOrder() {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = encoded
+
+        // Remember, if you don’t call resume() on your data task it won’t ever start,
+        // which is why I nearly always write the task and
+        // call resume before actually filling in the body.
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                print("No data in response: \(error?.localizedDescription ?? "Unknown error").")
+                return
+            }
+
+            if let decodedOrder = try? JSONDecoder().decode(Order.self, from: data) {
+                self.confirmationMessage = "Your order for \(decodedOrder.quantity) x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+                self.showingConfimation = true
+            } else {
+                print("Invalid response from server")
+            }
+        }.resume()
     }
 }
 
